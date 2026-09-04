6ce5ff93bb0a1502292cb1766d793bf1b772ccae
Samu&886 <mungais886@gmail.com>
2026-09-04 06:53:38 -0400

    creating pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-8">
-      <div className="bg-white p-8 rounded-xl shadow-md">
-        <h1 className="text-3xl font-bold text-gray-900">
-          Welcome to my store
-        </h1>
-
-        <p className="mt-4 text-gray-600">
-          Find what you love. Shop with confidence.
-        </p>
-
-        <button className="mt-6 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
-          Explore Products
-        </button>
-      </div>
-    </div>
+   <>
+   <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/about' element={<About />} />
+   </Routes>
+   </>
   );
 }
 
