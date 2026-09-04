aefa8b73ba6c1a19ce903a6cb0f325134048f2fa
Samu&886 <mungais886@gmail.com>
2026-09-04 07:25:34 -0400

    create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Navbar from "./Components/Navbar";
+import SignIn from "./pages/SignIn";
+import Contact from "./pages/Contact";
 function App() {
   return (
    <>
+   <Navbar />
    <Routes>
     <Route path='/' element={<Home />} />
     <Route path='/about' element={<About />} />
+    <Route path='/signin' element={<SignIn />} />
+    <Route path='/contact' element={<Contact />} />
    </Routes>
    </>
   );
