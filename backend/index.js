d76e719fdc98c642d336787fa6863fd2cf3c73a6
Samu&886 <mungais886@gmail.com>
2026-09-05 11:31:35 -0400

    connecting database to the server

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
+import "dotenv/config"
 import express from "express";
+import mongoose from "mongoose";
+
+
+mongoose.connect(process.env.MONGO)
+.then(() =>{
+    console.log("Connected to database");
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+
 
 const App = express();
 
