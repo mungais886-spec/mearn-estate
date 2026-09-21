59e6e55d928e105404875c42e0664736e823cbf3
Samu&886 <mungais886@gmail.com>
2026-09-21 06:41:41 -0400

    adding redux  persist

--- frontend/commerce/src/main.jsx
+++ frontend/commerce/src/main.jsx
@@ -3,15 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { store } from './redux/Store.js'
+import { persistor,store } from './redux/Store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
       <StrictMode>
         <App />
       </StrictMode>
     </BrowserRouter>
+    </PersistGate>
   </Provider>
 )
